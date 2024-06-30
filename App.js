4df437b134bd09aea2da3b9b753338a7ef0bedb6
Jennifer Akosua Sunkwa <jasunkwa@st.ug.edu.gh>
2024-06-30 16:33:14 +0000

    Increased fontsize to 24

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Jennifer Akosua Sunkwa</Text>
+      <Text style={styles.text}>My name is Jennifer Akosua Sunkwa</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 24,
+  }
 });
