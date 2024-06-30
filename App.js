2ab1ee809eceaf274f979880297c0baccf7bd01f
Jennifer Akosua Sunkwa <jasunkwa@st.ug.edu.gh>
2024-06-30 16:36:45 +0000

    Applied bold style to my name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Jennifer Akosua Sunkwa</Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldText}>Jennifer Akosua Sunkwa</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +19,9 @@ const styles = StyleSheet.create({
   },
   text:{
     fontSize: 24,
-  }
+    color: 'white',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
 });
